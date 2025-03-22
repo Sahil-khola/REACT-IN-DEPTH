f2826706d2e97c06d6f6e72153323ee7f8bd51ac
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-03-22 22:47:14 +0530

    REACT

---
+++ 10 React Router/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import React_Router from './component/React_Router'
+
+const App = () => {
+  return (
+    <div>
+      <React_Router/>
+    </div>
+  )
+}
+
+export default App
